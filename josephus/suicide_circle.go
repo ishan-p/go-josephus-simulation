fba904e97ae45c8c3fe641da57c3d3c1d4101e7c
Ishan <ishan@one2n.in>
2021-06-03 16:30:22 +0530

    Add function to recursively kill soldiers one by one

--- josephus/suicide_circle.go
+++ josephus/suicide_circle.go
@@ -12,3 +12,26 @@ func (circle *SuicideCircle) Init(soldiers []Soldier, hops int) {
 	circle.killed = make([]Soldier, 0, len(soldiers))
 	circle.hops = hops
 }
+
+func (circle *SuicideCircle) Kill(hops int) {
+	var toBeKilledSoldier Soldier
+	var survived []Soldier
+	survivors := len(circle.survivors)
+	if survivors == 1 {
+		circle.josephus = circle.survivors[0]
+		return
+	}
+	if hops > survivors {
+		survived = append(circle.survivors[survivors-1:], circle.survivors[:survivors-1]...)
+		hops = hops - (survivors - 1)
+		circle.survivors = survived
+	} else {
+		killIndex := hops - 1
+		toBeKilledSoldier = circle.survivors[killIndex]
+		circle.killed = append(circle.killed, toBeKilledSoldier)
+		survived = append(circle.survivors[killIndex+1:], circle.survivors[:killIndex]...)
+		circle.survivors = survived
+		hops = circle.hops
+	}
+	circle.Kill(hops)
+}
